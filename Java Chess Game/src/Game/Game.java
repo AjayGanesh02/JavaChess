3bf6fe7de718adb6c89cefb2b4ab42caf26c44fb
AjayGanesh02 <cooltechajay11@gmail.com>
2019-04-14 16:26:24 -0400

    Some more basic setup with Game and Piece
    -Cleaned up the Piece constructor
    -Worked on getting player names in the Board class for future use since
    that's going to be relatively easy

--- Java Chess Game/src/Game/Game.java
+++ Java Chess Game/src/Game/Game.java
@@ -1,9 +1,23 @@
 package Game;
+import java.util.Scanner;
 
 public class Game {
 
+	public Board gameBoard = new Board();
+	public static Player white;
+	public static Player black;
+	
+	static Scanner scan = new Scanner(System.in);
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		//Gets the names of the players
+		System.out.println("Enter the name of the player who will play white.");
+		//white.playerName = scan.next();
+		System.out.println("Enter the name of the player who will play black.");
+		//black.playerName = scan.next();
+		
 
 	}
 
--- Java Chess Game/src/Pieces/Piece.java
+++ Java Chess Game/src/Pieces/Piece.java
@@ -9,12 +9,19 @@ public abstract class Piece {
 	//The owner of each piece
 	public Player owner;
 	
+	//The type of each piece
+	public pieceType m_type;
+	
 	//Constructor for pieces
-	public Piece(Pair coord, Player own) {
+	public Piece(pieceType type, Pair coord, Player own) {
 		
+		m_type = type;
 		owner = own;
+		place = coord;
 	}
 	
 	//Abstract method to take in an end location and validate it for each piece
 	public abstract boolean isValidPath(Pair endLocation);
+	
+	
 }
