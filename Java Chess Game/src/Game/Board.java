f82bcde9f098ca80502e94beee4ab3ca6f44dc57
AjayGanesh02 <cooltechajay11@gmail.com>
2019-04-21 21:47:38 -0400

    Window setup
    Learned and added the basics of making a window, and put it in the board
    class

--- Java Chess Game/src/Game/Board.java
+++ Java Chess Game/src/Game/Board.java
@@ -1,9 +1,36 @@
 package Game;
-import javax.swing.JFrame;
+import java.awt.*;
+import javax.swing.*; 
+
 
 public class Board  {
 	
-	JFrame board = new JFrame("Java Chess by Ajay Ganesh");
+	private static void createWindow() {
+		
+		//Creates the window and closes the app on exiting the window
+		JFrame frame = new JFrame("Java Chess"); 
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+		
+		//Makes a label introducing the game
+		JLabel textLabel = new JLabel("Welcome to Java Chess!",SwingConstants.CENTER); 
+		textLabel.setPreferredSize(new Dimension(300, 100)); 
+		
+		//Displays the label in the window
+		frame.getContentPane().add(textLabel, BorderLayout.CENTER);
+		
+		//Display the window
+		frame.setLocationRelativeTo(null); 
+		
+		//Sets window size according to what's in it
+		frame.pack(); 
+		
+		frame.setVisible(true); 
+		
+	} 
+	
+	public static void main(String[] args) {
+		createWindow();
+		} 
 	
 	
 	
